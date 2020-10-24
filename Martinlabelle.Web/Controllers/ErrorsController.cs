01c1efd62efdfe18371b8faa6b93868eb257e689
Martin Labelle <martin.labelle1@gmail.com>
2020-10-24 14:51:13 -0400

    début intéressant

---
+++ Martinlabelle.Web/Controllers/ErrorsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Martinlabelle.Web.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        [Route("Error/500")]
+        public IActionResult Error500()
+        {
+            var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionFeature != null)
+            {
+                Logger.Error(exceptionFeature.Error, "Error 500");
+
+                ViewBag.ErrorMessage = exceptionFeature.Error.Message;
+                ViewBag.RouteOfException = exceptionFeature.Path;
+            }
+
+            return View();
+        }
+    }
+}
